dad0986e4bb0d1d494537169008653ef9ad8f649
Felipe Dias <adfelipealmeida@gmail.com>
2022-08-15 08:38:32 -0300

    iniciando uma lista de todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+
+  constructor() {
+    // chamado toda vez que a classe inicia
+    this.todos.push('passear com o cachorro');
+    this.todos.push('ir ao mercado');
+    this.todos.push('cortar o cabelo');
+  }
 }
